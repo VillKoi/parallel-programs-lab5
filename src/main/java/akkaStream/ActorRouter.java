4edfd721a5372c2ec14ecd148a3b48c5883677da
Вольнова Татьяна Михайловна <tmvolno1@mts.ru>
2021-12-10 23:22:22 +0300

    Scripted auto-commit on change (2021-12-10 23:22:22) by gitwatch.sh

--- src/main/java/akkaStream/ActorRouter.java
+++ src/main/java/akkaStream/ActorRouter.java
@@ -1,17 +1,26 @@
 package akkaStream;
 
+import akka.NotUsed;
 import akka.http.javadsl.marshallers.jackson.Jackson;
+import akka.http.javadsl.model.HttpRequest;
+import akka.http.javadsl.model.HttpResponse;
+import akka.http.javadsl.model.Query;
 import akka.http.javadsl.model.StatusCodes;
 import akka.http.javadsl.server.Route;
 import akka.pattern.Patterns;
+import akka.stream.ActorMaterializer;
+import akka.stream.javadsl.Flow;
 import scala.concurrent.Future;
 
 import java.util.ArrayList;
+import java.util.Queue;
 
 import static akka.http.javadsl.server.Directives.*;
 
 public class ActorRouter {
 
+    private static final String URL_QUERY = "url";
+    private static final String REQUEST_NUMBER_QUERY = "url";
 
     public Route createRouter() {
         return route(
@@ -32,5 +41,17 @@ public class ActorRouter {
                                             return complete(StatusCodes.OK);
                                         })))
                 ));
+    }
+
+    public Flow<HttpRequest, HttpResponse, NotUsed> createFlow(ActorMaterializer materializer) {
+        Flow.of(HttpRequest.class).map(
+                request -> {
+                    Query query = request.getUri().query();
+                    String url  = query.get(URL_QUERY).toString();
+                    int requestNumber = Integer.parseInt(query.get(REQUEST_NUMBER_QUERY).toString());
+                    
+
+                }
+        )
     }
 }
