3d042eaa28b972d670e9d8cd6e07860a357a233d
Вольнова Татьяна Михайловна <tmvolno1@mts.ru>
2021-12-10 23:34:44 +0300

    Scripted auto-commit on change (2021-12-10 23:34:44) by gitwatch.sh

--- src/main/java/akkaStream/ActorRouter.java
+++ src/main/java/akkaStream/ActorRouter.java
@@ -49,7 +49,7 @@ public class ActorRouter {
                     Query query = request.getUri().query();
                     String url  = query.get(URL_QUERY).toString();
                     Integer requestNumber = Integer.parseInt(query.get(REQUEST_NUMBER_QUERY).toString());
-                    Pair<String, Integer> = new Pair<>(url, requestNumber);
+                    Pair<String, Integer> result = new Pair<>(url, requestNumber);
 
                 }).mapAsync(10, )
         )
