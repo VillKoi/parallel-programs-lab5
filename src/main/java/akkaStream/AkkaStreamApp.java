7475ecec60206b63538038e30e0a8968847d389c
Вольнова Татьяна Михайловна <tmvolno1@mts.ru>
2021-12-10 19:14:50 +0300

    Scripted auto-commit on change (2021-12-10 19:14:50) by gitwatch.sh

--- src/main/java/akkaStream/AkkaStreamApp.java
+++ src/main/java/akkaStream/AkkaStreamApp.java
@@ -1,4 +1,31 @@
 package main.java.akkaStream;
 
+import java.io.IOException;
+
+import akka.actor.ActorSystem;
+import akka.stream.ActorMaterializer;
+
+import akka.http.javadsl.model.HttpRequest;
+import akka.http.javadsl.model.HttpResponse;
+
 public class AkkaStreamApp {
+    public static void main(String[] args) throws IOException {
+        System.out.println("start!");
+        ActorSystem system = ActorSystem.create("routes");
+        
+        final Http http = Http.get(system);
+        final ActorMaterializer materializer = ActorMaterializer.create(system);
+        final Flow<HttpRequest, HttpResponse, NotUsed> routeFlow = router.flow(system, materializer);
+        final CompletionStage<ServerBinding> binding = http.bindAndHandle(
+                routeFlow,
+                ConnectHttp.toHost("localhost", 8080),
+                materializer
+        );
+        System.out.println("Server online at http://localhost:8080/\nPress RETURN to stop...");
+        System.in.read();
+        binding
+                .thenCompose(ServerBinding::unbind)
+                .thenAccept(unbound -> system.terminate()); // and shutdown when done
+    }
+
 }
