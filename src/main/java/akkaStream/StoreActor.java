a1bc8f5abf4d6135f22b40e19f4f609740b805ea
Вольнова Татьяна Михайловна <tmvolno1@mts.ru>
2021-12-10 23:04:59 +0300

    Scripted auto-commit on change (2021-12-10 23:04:59) by gitwatch.sh

--- src/main/java/akkaStream/StoreActor.java
+++ src/main/java/akkaStream/StoreActor.java
@@ -31,12 +31,9 @@ public class StoreActor extends AbstractActor {
     };
 
     private Map<String, String> getResult(String packageID) {
-        Map<String, TestInformation>  testResults = storage.get(packageID);
+        TestInformation  testResults = storage.get(packageID);
         Map<String, String>  result = new HashMap<>();
-        for (TestInformation r: testResults.values()) {
-            System.out.println(r.toString());
-            result.put(r.getTestName(), r.toString());
-        }
+        
         return result;
     }
 }
